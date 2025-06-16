c656ff78bbc383e0693f35881ff940f31631fb13
Compilearner <ayushibaliyan769@gmail.com>
2025-06-16 17:49:02 +0530

    Remove frontend node_modules from Git tracking

--- Backend/server.js
+++ Backend/server.js
@@ -5,11 +5,13 @@ import authRoutes from "./Routes/authRoutes.js";
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import compression from "compression";
-
+import path from "path";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 //Middlware
 app.use(cookieParser());
@@ -25,6 +27,14 @@ app.use(cors({
 
 // Routes to server
 app.use("/api", authRoutes);
+
+if(process.env.NODE_ENV === "production"){
+   app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+
+   app.get("*", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+   })
+}
 
 // Connect to Database and Server
 app.listen(PORT, ()=>{
